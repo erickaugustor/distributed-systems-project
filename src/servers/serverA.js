14c58fc63145b4ec0091b33d4c64f02ea86b9298
erick.augustor <erick.augustoramos@gmail.com>
2019-04-22 00:05:44 -0300

    SD002: Adicionando o Heartbeat e mapeamento de serviços

--- src/servers/serverA.js
+++ src/servers/serverA.js
@@ -1,9 +1,16 @@
-var io = require('socket.io-client');
-
-var socket = io.connect('http://localhost:3000', {reconnect: true});
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000', {reconnect: true});
+const services = require('../services/servicesForServers');
 
 socket.on('connect', (socket) => { 
   console.log('Connected!');
 });
 
 socket.emit('imServer', ['servicoOne', 'servicoTwo']);
+
+
+const heartBeating = () => {
+  socket.emit('imAlive', ['servicoOne', 'servicoTwo']);
+};
+
+setInterval(heartBeating, 1000);
